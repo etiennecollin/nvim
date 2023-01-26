b95c333fb48818b4f0da73f963df2ae82ad2eb4e
Etienne Collin <collin.etienne.contact@gmail.com>
2023-01-26 00:25:30 -0500

    Removed transparency

--- lua/etiennecollin/config/dracula.lua
+++ lua/etiennecollin/config/dracula.lua
@@ -4,8 +4,8 @@ function setTheme(theme)
     vim.cmd.colorscheme(theme)
 
     -- Set transparency
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 setTheme()
