7a51bc435b7c27183a30fca0a93d511a7b5be7eb
Etienne Collin <collin.etienne.contact@gmail.com>
2023-01-26 21:18:42 -0500

    Update remap.lua

--- lua/etiennecollin/remap.lua
+++ lua/etiennecollin/remap.lua
@@ -1,9 +1,31 @@
 vim.g.mapleader = " "
 
--- Example remap such that they appear in whichkey
--- vim.keymap.set("n", "gq", "<cmd>lua vim.lsp.buf.formatting()<CR>", {
---     desc = "format document [LSP]"
--- })
-vim.keymap.set({"n", "i", "t"}, "<F2>", "<cmd>ToggleTermToggleAll<cr>", {
-    desc = "Toggle all terminals"
+-- Window navigation
+vim.keymap.set({"n"}, "<C-h>", "<C-w>h", {
+    desc = "Navigate window left"
+})
+vim.keymap.set({"n"}, "<C-j>", "<C-w>j", {
+    desc = "Navigate down"
+})
+vim.keymap.set({"n"}, "<C-k>", "<C-w>k", {
+    desc = "Navigate up"
+})
+vim.keymap.set({"n"}, "<C-l>", "<C-w>l", {
+    desc = "Navigate right"
+})
+
+-- Terminal
+-- F14 = Shift + F1
+vim.keymap.set({"n", "i", "v", "t"}, "<F14>", "<cmd>ToggleTermToggleAll<cr>", {
+    desc = "Toggle terminals"
+})
+
+-- Commenting
+vim.keymap.set({"v"}, "<C-/>", "<plug>NERDCommenterToggle", {
+    desc = "Toggle commenting"
+})
+
+-- Exit insert mode
+vim.keymap.set({"i"}, "jk", "<esc>", {
+    desc = "Exit insert mode"
 })
