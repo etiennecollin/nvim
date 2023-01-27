c5cbac151a274c297ceccab759624d7e5a3236bf
Etienne Collin <collin.etienne.contact@gmail.com>
2023-01-26 21:15:33 -0500

    Create nvimtree.lua

---
+++ lua/etiennecollin/config/nvimtree.lua
@@ -0,0 +1,18 @@
+require("nvim-tree").setup({
+    sort_by = "case_insensitive",
+    view = {
+        adaptive_size = true,
+        -- mappings = {
+        --     list = {{
+        --         key = "u",
+        --         action = "dir_up"
+        --     }}
+        -- }
+    },
+    renderer = {
+        group_empty = true
+    },
+    filters = {
+        dotfiles = true
+    }
+})
