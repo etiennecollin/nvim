a3349ded6128e51a1dff841185a0ba0eedfeb84c
Etienne Collin <collin.etienne.contact@gmail.com>
2023-01-31 00:38:55 -0500

    Create fonts.lua

---
+++ lua/etiennecollin/snippets/tex/fonts.lua
@@ -0,0 +1,28 @@
+return {
+    -- Examples of Greek letter snippets, autotriggered for efficiency
+    s({ trig = ";a", snippetType = "autosnippet" },
+        {
+            t("\\alpha"),
+        }
+    ),
+    s({ trig = ";b", snippetType = "autosnippet" },
+        {
+            t("\\beta"),
+        }
+    ),
+    s({ trig = ";d", snippetType = "autosnippet" },
+        {
+            t("\\delta"),
+        }
+    ),
+    s({ trig = ";g", snippetType = "autosnippet" },
+        {
+            t("\\gamma"),
+        }
+    ),
+    s({ trig = ";l", snippetType = "autosnippet" },
+        {
+            t("\\lambda"),
+        }
+    ),
+}
