ef352bd151f30310dc8312018bfac68b6b0ff4db
Etienne Collin <collin.etienne.contact@gmail.com>
2023-02-15 23:17:44 -0500

    Started creating snippet conditions

--- lua/etiennecollin/config/luasnip.lua
+++ lua/etiennecollin/config/luasnip.lua
@@ -1,12 +1,33 @@
-local M = {}
-
--- Be sure to explicitly define these LuaSnip node abbreviations!
 local ls = require("luasnip")
 local sn = ls.snippet_node
 local i = ls.insert_node
+tex_utils = {}
+
+-- Check if snippet to expand is in mathzone
+tex_utils.in_mathzone = function() -- math context detection
+    return vim.fn["vimtex#syntax#in_mathzone"]() == 1
+end
+
+-- Check if snippet to expand is in comment
+tex_utils.in_comment = function() -- comment detection
+    return vim.fn["vimtex#syntax#in_comment"]() == 1
+end
+
+-- Check if snippet to expand is in text
+tex_utils.in_text = function()
+    return not tex_utils.in_mathzone() and not tex_utils.in_comment()
+end
+
+---------------------------------
+-- Doesn't work for some reason???
+tex_utils.in_env = function(name) -- generic environment detection
+    local x, y = unpack(vim.fn["vimtex#env#is_inside"](name))
+    return x ~= "0" and y ~= "0"
+end
+---------------------------------
 
 -- Get content of visual selection
-function M.get_visual(args, parent)
+tex_utils.get_visual = function(args, parent)
     if (#parent.snippet.env.SELECT_RAW > 0) then
         return sn(nil, i(1, parent.snippet.env.SELECT_RAW))
     else
@@ -14,15 +35,12 @@ function M.get_visual(args, parent)
     end
 end
 
-function M.in_text()
-    return not in_mathzone() and not in_comment()
-end
-
-function M.begins_line()
+-- Check if snippet to expand is at the beginning of a line
+tex_utils.begins_line = function()
     local cur_line = vim.api.nvim_get_current_line()
     -- Checks if the current line consists of whitespace and then the snippet
     -- TODO: Fix limitation that the snippet cannot contain whitespace itself
     return #cur_line == #string.match(cur_line, "%s*[^%s]+")
 end
 
-return M
+return tex_utils
