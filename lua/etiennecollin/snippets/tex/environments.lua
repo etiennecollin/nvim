48b6dd6230def3c10e1902c5451dd2a37e6577ee
Etienne Collin <collin.etienne.contact@gmail.com>
2023-02-15 23:18:08 -0500

    Added snippet for example/subexample environments

---
+++ lua/etiennecollin/snippets/tex/environments.lua
@@ -0,0 +1,32 @@
+local tex_utils = require("etiennecollin.config.luasnip")
+
+return {
+    s({ trig = "example"},
+        fmta(
+            [[
+            \begin{example}{<>}
+                <>
+            \end{example}
+            ]],
+            {
+                i(1),
+                i(0),
+            }
+        )
+    ),
+    s({ trig = "subexample"},
+        fmta(
+            [[
+            \begin{subexample}{<>}
+                <>
+            \end{subexample}
+            ]],
+            {
+                i(1),
+                i(0),
+            }
+        ),
+        { condition = tex_utils.in_env("example") }
+    ),
+
+}
