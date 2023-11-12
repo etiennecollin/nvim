ee81627172229bc0ca610b826d765433b65fdac7
Etienne Collin <collin.etienne.contact@gmail.com>
2023-11-11 19:30:42 -0500

    Update remap.lua

--- lua/etiennecollin/core/remap.lua
+++ lua/etiennecollin/core/remap.lua
@@ -16,22 +16,41 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- Apparently the greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+-- Apparently the greatest remaps ever
+vim.keymap.set("x", "<leader>p", [["_dP]], {
+	desc = "Delete and paste (keep clipboard)",
+})
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], {
+	desc = "Yank selection to system clipboard",
+})
+vim.keymap.set("n", "<leader>Y", [["+Y]], {
+	desc = "Yank whole line to system clipboard",
+})
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], {
+	desc = "Delete without yank",
+})
+
+-- I'll get cancelled too...
+vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Insert mode escape" })
 
 -- Window navigation
 vim.keymap.set("n", "<C-h>", "<C-w>h", {
 	desc = "Navigate window left",
 })
 vim.keymap.set("n", "<C-j>", "<C-w>j", {
-	desc = "Navigate down",
+	desc = "Navigate window down",
 })
 vim.keymap.set("n", "<C-k>", "<C-w>k", {
-	desc = "Navigate up",
+	desc = "Navigate window up",
 })
 vim.keymap.set("n", "<C-l>", "<C-w>l", {
-	desc = "Navigate right",
+	desc = "Navigate window right",
 })
+
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- Terminal
 vim.keymap.set({ "n", "i", "v", "t" }, "<S-F1>", "<cmd>ToggleTermToggleAll<cr>", {
