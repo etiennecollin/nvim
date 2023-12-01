bd14b22b2eb6db421683797fcb75c5368d9fbc84
Etienne Collin <collin.etienne.contact@gmail.com>
2023-12-01 17:15:23 -0500

    Create todo-comments.lua

---
+++ lua/etiennecollin/plugins/todo-comments.lua
@@ -0,0 +1,6 @@
+return {
+	"folke/todo-comments.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = { "nvim-lua/plenary.nvim", "folke/trouble.nvim", "nvim-telescope/telescope.nvim" },
+	config = true,
+}
