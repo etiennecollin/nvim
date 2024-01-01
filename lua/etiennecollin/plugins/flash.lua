87405956d1e1cf60af75b4f312ff49ca8ea79bb9
Etienne Collin <collin.etienne.contact@gmail.com>
2024-01-01 14:31:24 -0500

    Moved from leap to flash

---
+++ lua/etiennecollin/plugins/flash.lua
@@ -0,0 +1,47 @@
+return {
+	"folke/flash.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		labels = "asdfghjklqwertyuiopzxcvbnm",
+		mode = "exact",
+		modes = {
+			char = {
+				enabled = false,
+			},
+		},
+	},
+	keys = {
+		{
+			"s",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"S",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "Flash Treesitter",
+		},
+		{
+			"r",
+			mode = "o",
+			function()
+				require("flash").remote()
+			end,
+			desc = "Remote Flash",
+		},
+		{
+			"R",
+			mode = { "o", "x" },
+			function()
+				require("flash").treesitter_search()
+			end,
+			desc = "Treesitter Search",
+		},
+	},
+}
