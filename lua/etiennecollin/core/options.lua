7c732e84f6e2dc7dee27e63e2ca523faefc59952
Etienne Collin <collin.etienne.contact@gmail.com>
2024-01-09 00:13:17 -0500

    Update options.lua

--- lua/etiennecollin/core/options.lua
+++ lua/etiennecollin/core/options.lua
@@ -1,13 +1,23 @@
 -- Setting global options
 
+-- Set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
 require("etiennecollin.utils").default_theme = "sonokai"
 
+-- Set cursor options and line highlighting
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
 vim.opt.cursorline = true
+vim.opt.colorcolumn = { "80", "120" }
 
+-- Enable incrementing hex numbers and letters
+vim.api.nvim_set_option("nrformats", "hex,alpha")
+
+-- Enable numbers and relative numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Set indentation
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -16,35 +26,34 @@ vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 
+-- Set search options
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Set how splits open
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
+-- Enable line wrapping
 vim.opt.wrap = true
 
+-- Enable hidden buffers
 vim.opt.hidden = true
 
+-- Backup and undo settings
 -- vim.opt.swapfile = false
 -- vim.opt.backup = false
 -- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 -- vim.opt.undofile = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
--- Set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
+-- Other settings
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
-
 vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = { "80", "120" }
-vim.api.nvim_set_option("nrformats", "hex,alpha")
 
 -- Vertically center document when entering insert mode
 vim.cmd("autocmd InsertEnter * norm zz")
