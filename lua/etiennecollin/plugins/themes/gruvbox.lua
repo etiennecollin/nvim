9321b0a30ad73a8f9042fd872f2e9b9ba9e90eee
Etienne Collin <collin.etienne.contact@gmail.com>
2024-01-09 00:09:24 -0500

    Create gruvbox.lua

---
+++ lua/etiennecollin/plugins/themes/gruvbox.lua
@@ -0,0 +1,10 @@
+return {
+	"sainnhe/gruvbox-material",
+	name = "gruvbox-material",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.g.gruvbox_material_foreground = "original"
+		vim.g.gruvbox_material_background = "medium"
+	end,
+}
