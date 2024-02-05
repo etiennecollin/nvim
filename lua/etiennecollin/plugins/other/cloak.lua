82306edee3b9d7c1444620fb4ce4ca824eda3f4d
Etienne Collin <collin.etienne.contact@gmail.com>
2024-02-05 14:38:23 -0500

    Update cloak.lua

--- lua/etiennecollin/plugins/other/cloak.lua
+++ lua/etiennecollin/plugins/other/cloak.lua
@@ -4,13 +4,14 @@ return {
 	opts = {
 		enabled = true,
 		cloak_character = "*",
-		cloak_length = 5,
+		cloak_length = 8,
 		-- The applied highlight group (colors) on the cloaking, see `:h highlight`.
 		highlight_group = "Comment",
 		patterns = {
 			{
 				file_pattern = {
 					".env*",
+					"*.asc",
 				},
 				cloak_pattern = { "=.+", ":.+" },
 			},
