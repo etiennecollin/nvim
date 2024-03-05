a1ef9283314385c9205f557dee7c2d7a6cf00863
Etienne Collin <collin.etienne.contact@gmail.com>
2024-03-05 17:38:15 -0500

    Update molten.lua

--- lua/etiennecollin/plugins/language-specific/molten.lua
+++ lua/etiennecollin/plugins/language-specific/molten.lua
@@ -5,7 +5,7 @@ return {
 	build = ":UpdateRemotePlugins",
 	init = function()
 		vim.g.molten_image_provider = "image.nvim"
-		vim.g.molten_output_win_max_height = 20
+		vim.g.molten_output_win_max_height = 100
 		vim.g.molten_auto_open_output = false
 	end,
 }
