a08857ad37c34419024475ebe112516ce994bd28
Etienne Collin <collin.etienne.contact@gmail.com>
2024-03-05 03:35:50 -0500

    Updated quarto and otter configs

--- lua/etiennecollin/plugins/language-specific/otter.lua
+++ lua/etiennecollin/plugins/language-specific/otter.lua
@@ -1,9 +1,11 @@
 return {
 	"jmbuhr/otter.nvim",
 	ft = "quarto",
+	dependencies = { "neovim/nvim-lspconfig" },
 	opts = {
 		buffers = {
 			set_filetype = true,
 		},
+		handle_leading_whitespace = true,
 	},
 }
--- lua/etiennecollin/plugins/language-specific/quarto.lua
+++ lua/etiennecollin/plugins/language-specific/quarto.lua
@@ -1,10 +1,27 @@
 return {
 	"quarto-dev/quarto-nvim",
+	ft = "quarto",
+	dependencies = { "jmbuhr/otter.nvim" },
 	opts = {
 		lspFeatures = {
 			languages = { "r", "python", "julia", "bash", "html", "lua" },
+			diagnostics = {
+				triggers = { "BufWritePost", "BufEnter" },
+			},
+		},
+		codeRunner = {
+			enabled = true,
+			default_method = "molten",
+		},
+		keymap = {
+			-- set whole section or individual keys to `false` to disable
+			hover = "K",
+			definition = "gd",
+			type_definition = "gD",
+			rename = "<leader>rn",
+			format = "<leader>f",
+			references = "gr",
+			document_symbols = "gS",
 		},
 	},
-	ft = "quarto",
-	dependencies = { "jmbuhr/otter.nvim" },
 }
