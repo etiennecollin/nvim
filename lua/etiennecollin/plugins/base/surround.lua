26a91ae3a2bcac2091f1ff8457b6aa92b31299bc
Etienne Collin <collin.etienne.contact@gmail.com>
2024-03-13 01:03:20 -0400

    Fixed surround mappings

--- lua/etiennecollin/plugins/base/surround.lua
+++ lua/etiennecollin/plugins/base/surround.lua
@@ -2,5 +2,19 @@ return {
 	"kylechui/nvim-surround",
 	event = { "BufReadPre", "BufNewFile" },
 	version = "*",
-	config = true,
+	opts = {
+		keymaps = {
+			insert = false,
+			insert_line = false,
+			normal = "gs",
+			normal_cur = "gss",
+			normal_line = "gS",
+			normal_cur_line = "gSS",
+			visual = "gS",
+			visual_line = "gS",
+			delete = "ds",
+			change = "cs",
+			change_line = "cS",
+		},
+	},
 }
