46f47174f0978382ac1ffb4fa2ffe2cf3b424a45
Etienne Collin <collin.etienne.contact@gmail.com>
2024-03-13 01:38:38 -0400

    Update silicon.lua

--- lua/etiennecollin/plugins/other/silicon.lua
+++ lua/etiennecollin/plugins/other/silicon.lua
@@ -6,16 +6,24 @@ return {
 	end,
 	opts = {},
 	config = function()
-		local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+		local file = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
+		local fullname = vim.fn.fnamemodify(file, ":t")
+		local basename = vim.fn.fnamemodify(file, ":r")
+		local background_path = vim.fn.expand("~/github/dotfiles/wallpapers/ancient_bristlecone_pine_forest.jpg")
+
 		require("silicon").setup({
-			theme = "Monokai Extended",
 			font = "JetBrainsMono Nerd Font=34",
+			theme = "Monokai Extended",
+			background = nil,
+			background_image = background_path,
+			shadow_color = "#161618",
 			to_clipboard = true,
+			line_offset = 0,
 			window_title = function()
-				return filename
+				return fullname
 			end,
 			output = function()
-				return "./" .. os.date("!%Y_%m_%dT%H_%M_%S") .. filename .. "_code.png"
+				return "./" .. basename .. os.date("!%Y%m%dT%H%M%S") .. "_code.png"
 			end,
 		})
 	end,
