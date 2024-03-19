046c4ba05b4236eda8ed252c9bb37fb387fe5ee6
Etienne Collin <collin.etienne.contact@gmail.com>
2024-03-19 00:18:44 -0400

    Added FormatDisable/Enable commands

--- lua/etiennecollin/plugins/lsp/conform.lua
+++ lua/etiennecollin/plugins/lsp/conform.lua
@@ -29,7 +29,13 @@ return {
 				markdown = { "prettier" },
 				graphql = { "prettier" },
 			},
-			format_on_save = { timeout_ms = 500, lsp_fallback = true },
+			format_on_save = function(bufnr)
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { timeout_ms = 500, lsp_fallback = true }
+			end,
 			formatters = {
 				black = {
 					prepend_args = { "--line-length=120" },
@@ -45,6 +51,25 @@ return {
 
 		-- Install formatters with mason
 		require("mason-conform").setup()
+
+		-- Create commands to enable/disable autoformat-on-save
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>f", function()
 			conform.format({ lsp_fallback = true, async = false })
