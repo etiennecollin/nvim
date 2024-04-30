ace642663a6b13ecd72d7cf62299e0896ec10e14
etiennecollin <collin.etienne.contact@gmail.com>
2024-04-29 23:59:10 -0400

    Update crates.lua

--- lua/etiennecollin/plugins/language-specific/crates.lua
+++ lua/etiennecollin/plugins/language-specific/crates.lua
@@ -2,11 +2,6 @@ return {
 	"saecki/crates.nvim",
 	event = { "BufRead Cargo.toml" },
 	tag = "stable",
-	dependencies = { "nvim-lua/plenary.nvim", "nvimtools/none-ls.nvim" },
-	opts = {
-		null_ls = {
-			enabled = true,
-			name = "crates.nvim",
-		},
-	},
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = true,
 }
