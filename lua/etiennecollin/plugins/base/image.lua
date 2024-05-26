3626d106a57b2bf1e78e96025fc355c99e50b022
etiennecollin <collin.etienne.contact@gmail.com>
2024-05-26 03:22:57 -0400

    Disabled image.nvim for now
    Bug: magick rock not found

--- lua/etiennecollin/plugins/base/image.lua
+++ lua/etiennecollin/plugins/base/image.lua
@@ -1,5 +1,6 @@
 return {
 	"3rd/image.nvim",
+	enabled = false,
 	init = function()
 		package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
 		package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
