ca4c0df9421c014f993d1f9e3a649c61373a338b
etiennecollin <collin.etienne.contact@gmail.com>
2024-06-07 00:50:58 -0400

    Lazy loaded Spectre

--- lua/etiennecollin/plugins/base/spectre.lua
+++ lua/etiennecollin/plugins/base/spectre.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-pack/nvim-spectre",
 	dependencies = "nvim-lua/plenary.nvim",
+	cmd = "Spectre",
 	config = function()
 		require("etiennecollin.core.remaps_plugin").spectre()
 	end,
