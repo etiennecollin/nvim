e3a4932977713864f9b36c80a7aac29d12d8f18d
etiennecollin <collin.etienne.contact@gmail.com>
2024-09-12 16:44:49 -0400

    Added code folding

--- lua/etiennecollin/core/options.lua
+++ lua/etiennecollin/core/options.lua
@@ -94,6 +94,13 @@ vim.opt.hidden = true
 -- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- Setup folds
+vim.o.foldcolumn = "0"
+vim.o.foldenable = false
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldlevel = 99
+vim.o.foldmethod = "expr"
+
 -- Other settings
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
