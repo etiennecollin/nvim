708f9a3ae20c2e9939288f2e8b95c6a910dbc6e0
etiennecollin <collin.etienne.contact@gmail.com>
2024-10-28 20:45:40 -0400

    Fixed image.nvim image size and ft

--- lua/etiennecollin/plugins/extra/image.lua
+++ lua/etiennecollin/plugins/extra/image.lua
@@ -1,5 +1,6 @@
 return {
 	"3rd/image.nvim",
+	ft = { "markdown", "vimwiki", "html", "typst" },
 	dependencies = {
 		"leafo/magick",
 	},
@@ -8,27 +9,32 @@ return {
 		integrations = {
 			typst = {
 				enabled = true,
+				download_remote_images = true,
+				only_render_image_at_cursor = true,
 			},
 			html = {
 				enabled = true,
+				download_remote_images = true,
+				only_render_image_at_cursor = true,
 			},
 			markdown = {
 				enabled = true,
 				clear_in_insert_mode = false,
 				download_remote_images = true,
-				only_render_image_at_cursor = false,
-				filetypes = { "markdown", "vimwiki", "typst" },
+				only_render_image_at_cursor = true,
+				filetypes = { "markdown", "vimwiki" },
 			},
 		},
-		max_width = 100,
-		max_height = 20,
-		max_height_window_percentage = math.huge, -- this is necessary for a good experience
-		max_width_window_percentage = math.huge,
+		max_width = nil,
+		max_height = nil,
+		max_width_window_percentage = 50,
+		max_height_window_percentage = 50,
 		-- auto show/hide images when the editor gains/looses focus
 		editor_only_render_when_focused = false,
 		-- toggles images when windows are overlapped
-		window_overlap_clear_enabled = false,
+		window_overlap_clear_enabled = true,
+		window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
 		-- auto show/hide images in the correct Tmux window (needs visual-activity off)
-		tmux_show_only_in_active_window = true,
+		tmux_show_only_in_active_window = false,
 	},
 }
