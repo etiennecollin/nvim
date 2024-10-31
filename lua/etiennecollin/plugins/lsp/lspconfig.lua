9864b7c41c716c91e9d4644232e765897ad9f2eb
etiennecollin <collin.etienne.contact@gmail.com>
2024-10-31 15:14:35 -0400

    Fixed lspconfig on_attach callback

--- lua/etiennecollin/plugins/lsp/lspconfig.lua
+++ lua/etiennecollin/plugins/lsp/lspconfig.lua
@@ -51,7 +51,7 @@ return {
 		-- Setup handlers
 		-----------------------------------------------------------------------
 		local capabilities = require("etiennecollin.utils").get_lsp_capabilities()
-		local on_attach = require("etiennecollin.core.remaps_plugin").lsp()
+		local on_attach = require("etiennecollin.core.remaps_plugin").lsp
 
 		local default_handler = function(server_name)
 			require("lspconfig")[server_name].setup({
