4fecb6f58928915008176de81f1fefb031f777d9
etiennecollin <collin.etienne.contact@gmail.com>
2024-10-31 00:02:31 -0400

    Updated img-clip settings

--- lua/etiennecollin/plugins/extra/img-clip.lua
+++ lua/etiennecollin/plugins/extra/img-clip.lua
@@ -3,12 +3,40 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	opts = {
 		default = {
-			url_encode_path = true,
+			-- File and directory options
+			dir_path = "assets",
+			extension = "png",
+			file_name = "%Y-%m-%d-%H-%M-%S",
+			use_absolute_path = false,
 			relative_to_current_file = true,
+
+			-- Template options
+			template = "$FILE_PATH",
+			url_encode_path = true,
+			relative_template_path = true,
+			use_cursor_in_template = false,
+			insert_mode_after_paste = true,
+
+			-- Prompt options
+			prompt_for_file_name = true,
+			show_dir_path_in_prompt = false,
+
+			-- Image options
+			copy_images = true, -- When DnD, copy the image to the assets directory
+			download_images = true, -- Download images from the web to the assets directory
+
+			-- Drag and drop options
+			drag_and_drop = {
+				enabled = true,
+				insert_mode = true,
+			},
 		},
 		filetypes = {
 			quarto = {
 				template = "![$CURSOR]($FILE_PATH)",
+			},
+			markdown = {
+				download_images = true,
 			},
 		},
 	},
