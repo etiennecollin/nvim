c4c5070300b8d857fb98e42f296914aa641bd392
etiennecollin <collin.etienne.contact@gmail.com>
2024-12-11 00:23:00 -0500

    Toggleterm loads its mappings

--- lua/etiennecollin/plugins/core/toggleterm.lua
+++ lua/etiennecollin/plugins/core/toggleterm.lua
@@ -2,5 +2,8 @@ return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
 	cmd = { "ToggleTerm", "ToggleTermToggleAll", "TermExec" },
+	init = function()
+		require("etiennecollin.core.remaps_plugin").toggleterm()
+	end,
 	config = true,
 }
