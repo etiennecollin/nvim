50748b2c72055c4ad08e0c379bed19c5f4a29585
etiennecollin <collin.etienne.contact@gmail.com>
2025-04-01 18:38:35 -0400

    Set lua tabstop to 2

--- lua/etiennecollin/autocmds.lua
+++ lua/etiennecollin/autocmds.lua
@@ -20,7 +20,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 local augroup_indent = vim.api.nvim_create_augroup("etiennecollin-filetype-indent", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
 	group = augroup_indent,
-	pattern = { "javascript", "javascriptreact", "typst", "markdown", "json", "jsonc" },
+	pattern = { "javascript", "javascriptreact", "typst", "markdown", "json", "jsonc", "lua" },
 	callback = function()
 		vim.opt_local.tabstop = 2
 	end,
