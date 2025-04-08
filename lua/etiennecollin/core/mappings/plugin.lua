06e44eb59b5c8a9bf8b3fcd23266ffd8bcd3e0d6
etiennecollin <collin.etienne.contact@gmail.com>
2025-04-08 15:18:01 -0400

    Removed harpoon

--- lua/etiennecollin/core/mappings/plugin.lua
+++ lua/etiennecollin/core/mappings/plugin.lua
@@ -203,18 +203,6 @@ function M.grug_far()
 	vim.keymap.set("v", "<leader>S", ":GrugFarWithin<cr>", { desc = "GrugFarWithin" })
 end
 
-function M.harpoon()
-	local harpoon = require("harpoon")
-
-	vim.keymap.set("n", "<leader>a", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon" })
-	vim.keymap.set("n", "<leader>A", function() harpoon:list():add() end, { desc = "Add to Harpoon" })
-
-	vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon select 1" })
-	vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon select 2" })
-	vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon select 3" })
-	vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon select 4" })
-end
-
 function M.lsp(_, bufnr)
 	local map = function(keys, func, desc)
 		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
--- lua/etiennecollin/plugins/core/harpoon.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"theprimeagen/harpoon",
-	branch = "harpoon2",
-	dependencies = "nvim-lua/plenary.nvim",
-	config = function()
-		require("harpoon"):setup()
-		require("etiennecollin.core.mappings.plugin").harpoon()
-	end,
-}
