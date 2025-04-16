10a25dbba5d9045a62908d02e6fe2ab14ec495a2
etiennecollin <collin.etienne.contact@gmail.com>
2025-04-16 19:23:43 -0400

    Updated typst-preview config
    - Use tinymist binary from mason instead of downloading it

--- lua/etiennecollin/plugins/language-specific/typst-preview.lua
+++ lua/etiennecollin/plugins/language-specific/typst-preview.lua
@@ -1,10 +1,9 @@
 return {
 	"chomosuke/typst-preview.nvim",
 	ft = "typst",
-	build = function()
-		require("typst-preview").update()
-	end,
+	version = "1.*",
 	opts = {
+		dependencies_bin = { ["tinymist"] = "tinymist" }, -- Installed by mason
 		get_root = function()
 			return os.getenv("HOME")
 		end,
