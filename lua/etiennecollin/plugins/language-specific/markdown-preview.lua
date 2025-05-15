e978e14d2be191486eb5bcf818d5d1334159501d
etiennecollin <collin.etienne.contact@gmail.com>
2025-05-14 21:25:28 -0400

    Disabled auto_close for markdown-preview

--- lua/etiennecollin/plugins/language-specific/markdown-preview.lua
+++ lua/etiennecollin/plugins/language-specific/markdown-preview.lua
@@ -7,7 +7,7 @@ return {
 	end,
 	init = function()
 		vim.g.mkdp_auto_start = 0
-		vim.g.mkdp_auto_close = 1
+		vim.g.mkdp_auto_close = 0
 		vim.g.mkdp_echo_preview_url = 1
 		vim.g.mkdp_page_title = "${name}.md"
 	end,
