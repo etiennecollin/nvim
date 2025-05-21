4d11f7de684b2cd0067654692f1def0ed78098db
etiennecollin <collin.etienne.contact@gmail.com>
2025-05-20 23:07:21 -0400

    Cleanup

--- lua/etiennecollin/plugins/core/grug-far.lua
+++ lua/etiennecollin/plugins/core/grug-far.lua
@@ -1,7 +1,6 @@
 return {
   "MagicDuck/grug-far.nvim",
   cmd = { "GrugFar", "GrugFarWithin" },
-  opts = {},
   init = function()
     require("etiennecollin.core.mappings.plugin").grug_far()
   end,
--- lua/etiennecollin/plugins/core/mini.lua
+++ lua/etiennecollin/plugins/core/mini.lua
@@ -11,8 +11,14 @@ return {
           a = { "@block.outer", "@conditional.outer", "@loop.outer" },
           i = { "@block.inner", "@conditional.inner", "@loop.inner" },
         }, {}),
-        F = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-        c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+        F = ai.gen_spec.treesitter({
+          a = "@function.outer",
+          i = "@function.inner",
+        }, {}),
+        c = ai.gen_spec.treesitter({
+          a = "@class.outer",
+          i = "@class.inner",
+        }, {}),
       },
     })
 
