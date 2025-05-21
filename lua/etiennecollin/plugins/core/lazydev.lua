2464e7c361b3a710553089278493dd8421eedf66
etiennecollin <collin.etienne.contact@gmail.com>
2025-05-20 23:05:27 -0400

    Added lazydev libraries

--- lua/etiennecollin/plugins/core/lazydev.lua
+++ lua/etiennecollin/plugins/core/lazydev.lua
@@ -4,6 +4,15 @@ return {
   opts = {
     library = {
       "nvim-dap-ui",
+      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      { path = "LazyVim", words = { "LazyVim" } },
+      { path = "snacks.nvim", words = { "Snacks" } },
+      { path = "lazy.nvim", words = { "LazyVim" } },
+    },
+    integrations = {
+      lspconfig = true,
+      cmp = false,
+      coq = false,
     },
   },
 }
