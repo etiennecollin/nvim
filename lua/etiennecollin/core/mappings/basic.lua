c015dd66581d8cfb9a3c1b764a0016c5d6d4cb09
etiennecollin <collin.etienne.contact@gmail.com>
2025-06-18 18:49:38 -0400

    Improved keymaps
    - Window navigation
    - Window management
    - Buffer, Window and Tab management
    - Better <leader>q behaviour
    - Other small changes

--- lua/etiennecollin/core/mappings/basic.lua
+++ lua/etiennecollin/core/mappings/basic.lua
@@ -77,6 +77,21 @@ vim.keymap.set({ "n", "v", "i", "t" }, "<c-right>", "<c-w>l", {
   desc = "Navigate window right",
 })
 
+-- Window placement with arrows
+-- On my COLEMAK mod-DH layout, I have a layer with the arrows in place of hjkl
+vim.keymap.set({ "n", "v", "i", "t" }, "<c-s-left>", "<c-w>H", {
+  desc = "Move window left",
+})
+vim.keymap.set({ "n", "v", "i", "t" }, "<c-s-down>", "<c-w>J", {
+  desc = "Move window down",
+})
+vim.keymap.set({ "n", "v", "i", "t" }, "<c-s-up>", "<c-w>K", {
+  desc = "Move window up",
+})
+vim.keymap.set({ "n", "v", "i", "t" }, "<c-s-right>", "<c-w>L", {
+  desc = "Move window right",
+})
+
 -- Window resizing
 vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]], { desc = "Make window bigger vertically" })
 vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]], { desc = "Make window smaller vertically" })
@@ -84,27 +99,49 @@ vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]], { desc = "Make windo
 vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]], { desc = "Make window smaller horizontally" })
 
 -- Window splitting
-vim.keymap.set("n", "<leader>v", "<cmd>vsplit<cr>", { desc = "VSplit window" })
 vim.keymap.set("n", "<leader>h", "<cmd>split<cr>", { desc = "HSplit window" })
+vim.keymap.set("n", "<leader>v", "<cmd>vsplit<cr>", { desc = "VSplit window" })
 
 -- Buffers
-vim.keymap.set("n", "<leader>bc", "<cmd>close<cr>", { desc = "Close buffer" })
 vim.keymap.set("n", "<leader>bD", "<cmd>%bd|e#|bd#<cr>", { desc = "Delete all buffers" })
-vim.keymap.set("n", "<leader>bn", "<cmd>ene | startinsert<cr>", { desc = "New buffer" })
+vim.keymap.set("n", "<leader>bF", "<cmd>ene | startinsert<cr>", { desc = "New fullscreen buffer" })
+vim.keymap.set("n", "<leader>bN", "<cmd>bp<cr>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>bd", "<cmd>bd<cr>", { desc = "Delete buffer" })
+vim.keymap.set("n", "<leader>bf", "<cmd>tab split<cr>", { desc = "Open current buffer in fullscreen tab" })
+vim.keymap.set("n", "<leader>bn", "<cmd>bn<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", "<cmd>bp<cr>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>bs", "<cmd>new<cr>", { desc = "New split buffer" })
 
 -- Tabs
-vim.keymap.set("n", "<leader>Bc", "<cmd>tabclose<cr>", { desc = "Close tab" })
+vim.keymap.set("n", "<leader>Bc", "<cmd>tabc<cr>", { desc = "Close tab" })
 vim.keymap.set("n", "<leader>Bn", "<cmd>tabn<cr>", { desc = "Next tab" })
-vim.keymap.set("n", "<leader>Bo", "<cmd>tabnew<cr>", { desc = "Open tab" })
+vim.keymap.set("n", "<leader>Bo", "<cmd>tabe<cr>", { desc = "Open tab" })
 vim.keymap.set("n", "<leader>Bp", "<cmd>tabp<cr>", { desc = "Previous tab" })
 
 -- Saving and quitting
-vim.keymap.set("n", "<leader>q", "<cmd>bd<cr>", { desc = "Delete buffer" })
+vim.keymap.set("n", "<leader>q", function()
+  -- If it's a help buffer, just bdelete it
+  local buftype = vim.api.nvim_get_option_value("buftype", { buf = 0 })
+  if buftype == "help" then
+    vim.cmd("bdelete")
+    return
+  end
+
+  -- Try to close the current window
+  local ok = pcall(function()
+    vim.cmd("close")
+  end)
+
+  -- If it's the last window
+  if not ok then
+    vim.notify("Last window", vim.log.levels.INFO)
+  end
+end, { desc = "Close window" })
 vim.keymap.set("n", "<leader>Q", "<cmd>q<cr>", { desc = "Quit" })
-vim.keymap.set("n", "<leader>w", "<cmd>update!<cr>", { desc = "Save" })
-vim.keymap.set("n", "<leader>XE", "<cmd>set fileformat=unix<cr>", { desc = "Set file format to unix" })
+vim.keymap.set("n", "<leader>w", "<cmd>up!<cr>", { desc = "Save" })
 
 -- Utilities
+vim.keymap.set("n", "<leader>XE", "<cmd>set fileformat=unix<cr>", { desc = "Set file format to unix" })
 vim.keymap.set("n", "<leader>XX", "<cmd>!chmod +x %<cr>", { desc = "Make executable" })
-vim.keymap.set("n", "<leader>zm", "<cmd>Mason<cr>", { desc = "Mason" })
 vim.keymap.set("n", "<leader>zl", "<cmd>Lazy<cr>", { desc = "Lazy" })
+vim.keymap.set("n", "<leader>zm", "<cmd>Mason<cr>", { desc = "Mason" })
