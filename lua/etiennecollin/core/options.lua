405cc4de8dcf1ba9fdf1077bae0fcdf40f034f38
ecollin <etienne.collin@reactiondynamics.space>
2025-06-25 19:51:15 -0400

    Added neovide options

--- lua/etiennecollin/core/options.lua
+++ lua/etiennecollin/core/options.lua
@@ -176,3 +176,25 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 2000
 vim.opt.redrawtime = 1000
 vim.opt.showcmd = false
+
+--------------------------------------------------------------------------------
+-- Neovide
+--------------------------------------------------------------------------------
+if vim.g.neovide then
+  vim.opt.guifont = "Maple Mono NF:h14"
+  vim.g.neovide_scale_factor = 1.0
+  vim.g.neovide_hide_mouse_when_typing = false
+  vim.g.neovide_fullscreen = true
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.25)
+  end)
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.25)
+  end)
+end
