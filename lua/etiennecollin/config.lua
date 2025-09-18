260cb75738c4bdf2c9afbbdc66104a254173f42e
ecollin <etienne.collin@reactiondynamics.space>
2025-09-18 14:20:26 -0400

    Updated cpp/cmake lsp and formatting

--- lua/etiennecollin/config.lua
+++ lua/etiennecollin/config.lua
@@ -24,7 +24,7 @@ M.background_style = "dark"
 M.ensure_installed_treesitter = { "vim", "regex", "lua", "bash", "markdown", "markdown_inline", "comment" }
 
 --- The LSPs to be installed by Mason.
-M.ensure_installed_lsps = { "lua_ls", "clangd", "basedpyright", "bashls", "jdtls", "tinymist", "ts_ls" }
+M.ensure_installed_lsps = { "lua_ls", "clangd", "basedpyright", "bashls", "jdtls", "tinymist", "ts_ls", "neocmake" }
 
 --- The Linters to be installed by Mason.
 M.ensure_installed_linters = {
@@ -69,7 +69,6 @@ M.ensure_installed_daps = { "codelldb", "cpptools" }
 
 --- Decrease tabstop for certain filetypes
 M.reduced_tabstop = {
-  "cpp",
   "fpp",
   "javascript",
   "javascriptreact",
