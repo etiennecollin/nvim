b73a24b67964970bd5dcae827a88072c3d125f75
ecollin <etienne.collin@reactiondynamics.space>
2025-10-31 14:10:20 -0400

    Make dropbar only show filename and symbols

--- lua/etiennecollin/plugins/core/dropbar.lua
+++ lua/etiennecollin/plugins/core/dropbar.lua
@@ -1,7 +1,30 @@
 return {
   "Bekaboo/dropbar.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  config = function()
+  opts = {
+    bar = {
+      sources = function()
+        local sources = require("dropbar.sources")
+        local utils = require("dropbar.utils")
+        local filename = {
+          get_symbols = function(buff, win, cursor)
+            local path = sources.path.get_symbols(buff, win, cursor)
+            return { path[#path] }
+          end,
+        }
+        return {
+          filename,
+          utils.source.fallback({
+            sources.lsp,
+            sources.treesitter,
+            sources.markdown,
+          }),
+        }
+      end,
+    },
+  },
+  config = function(_, opts)
+    require("dropbar").setup(opts)
     require("etiennecollin.core.mappings.plugin").dropbar()
   end,
 }
