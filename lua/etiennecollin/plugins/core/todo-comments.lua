2ff07849bcf631c56892c5eb0bf4119541147166
ecollin <etienne.collin@reactiondynamics.space>
2025-10-31 14:11:24 -0400

    Dropped plenary dep for crates and todo-comments

--- lua/etiennecollin/plugins/core/todo-comments.lua
+++ lua/etiennecollin/plugins/core/todo-comments.lua
@@ -1,9 +1,6 @@
 return {
   "folke/todo-comments.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
   opts = {
     search = { pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]] },
     highlight = { pattern = [[.*<((KEYWORDS)%(\(.{-1,}\))?):]] },
--- lua/etiennecollin/plugins/language-specific/crates.lua
+++ lua/etiennecollin/plugins/language-specific/crates.lua
@@ -2,6 +2,5 @@ return {
   "saecki/crates.nvim",
   event = { "BufRead Cargo.toml" },
   tag = "stable",
-  dependencies = { "nvim-lua/plenary.nvim" },
   config = true,
 }
