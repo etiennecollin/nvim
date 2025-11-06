fb76fbee2662b54b2ab1d3446f7c28449131e323
ecollin <etienne.collin@reactiondynamics.space>
2025-11-06 18:18:17 -0500

    Fixed diffview layout

--- lua/etiennecollin/plugins/core/diffview.lua
+++ lua/etiennecollin/plugins/core/diffview.lua
@@ -2,4 +2,11 @@ return {
   "sindrets/diffview.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+  opts = {
+    view = {
+      merge_tool = {
+        layout = "diff3_mixed",
+      },
+    },
+  },
 }
