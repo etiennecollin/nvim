d4ceac483e1b0d982de504c5681689e4ddbddfd4
ecollin <etienne.collin@reactiondynamics.space>
2026-03-31 12:11:46 -0400

    refactor: replace basedpyright with ty

--- lua/etiennecollin/config.lua
+++ lua/etiennecollin/config.lua
@@ -25,8 +25,7 @@ M.ensure_installed_treesitter =
   { "vim", "regex", "lua", "bash", "markdown", "markdown_inline", "comment", "python", "c", "cpp" }
 
 --- The LSPs to be installed by Mason.
-M.ensure_installed_lsps =
-  { "lua_ls", "clangd", "basedpyright", "bashls", "tinymist", "ts_ls", "neocmake", "markdown_oxide" }
+M.ensure_installed_lsps = { "lua_ls", "clangd", "ty", "bashls", "tinymist", "ts_ls", "neocmake", "markdown_oxide" }
 
 --- The Linters to be installed by Mason.
 M.ensure_installed_linters = {
